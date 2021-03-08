c608a32f88912cee9fc01b6b173cdb8c6e88db3c
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-03-08 11:34:22 -0500

    Initial Commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,10 @@
+import Output from "./Output";
+
+const Counter = (props) => (
+    <>
+        <h1>The MOST Amazing Counting App EVAR!</h1>
+        <Output amount={42} />
+    </>
+);
+
+export default Counter;
